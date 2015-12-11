4fc7e2c13afcd9b612adceda2bc4c6349f1e84b2
Max Locher <maxxiedivine@gmail.com>
2015-12-11 11:34:08 -0700

    adding SetupGenerator

---
+++ SetupGenerator/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupGenerator
+{
+    class Card
+    {
+        public string cardName;
+        public string cardCost;
+        public string cardType;
+        public string cardText;
+        public string cardSet;
+
+        public string Name
+        {
+            get { return this.cardName; }
+            set { this.cardName = value; }
+        }
+
+        public string Cost
+        {
+            get { return this.cardCost; }
+            set { this.cardCost = value; }
+        }
+
+        public string Type
+        {
+            get { return this.cardCost; }
+            set { this.cardCost = value; }
+        }
+
+        public string Text
+        {
+            get { return this.cardText; }
+            set { this.cardText = value; }
+        }
+
+        public string Set
+        {
+            get { return this.cardText; }
+            set { this.cardText = value; }
+        }
+    }
+}
---
+++ SetupGenerator/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SetupGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CardName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CardName
+            // 
+            this.CardName.AutoSize = true;
+            this.CardName.Location = new System.Drawing.Point(109, 50);
+            this.CardName.Name = "CardName";
+            this.CardName.Size = new System.Drawing.Size(35, 13);
+            this.CardName.TabIndex = 0;
+            this.CardName.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.CardName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CardName;
+    }
+}
+
